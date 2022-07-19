bcd6c20a52b74f94aea3ac1d66508f0832428f7e
Alex Horsov <horusadeptus@gmail.com>
2022-07-19 12:37:58 +0600

    First commit

---
+++ src/main/java/horsov/Main.java
@@ -0,0 +1,33 @@
+package horsov;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class Main extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        log("INITIALIZING");
+    }
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().write("Service enter\n");
+        super.service(req, resp);
+        resp.getWriter().write("Service exit\n");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        resp.getWriter().write("DoGet\n");
+    }
+
+    @Override
+    public void destroy() {
+        log("DESTROY");
+    }
+}
