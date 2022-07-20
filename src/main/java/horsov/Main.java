9743154b7cc97fdaa607ed062f86effd73446130
Alex Horsov <horusadeptus@gmail.com>
2022-07-20 17:55:53 +0600

    Replace annotation

--- src/main/java/horsov/Main.java
+++ src/main/java/horsov/Main.java
@@ -2,11 +2,13 @@ package horsov;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/servlet")
 public class Main extends HttpServlet {
     @Override
     public void init(ServletConfig config) throws ServletException {
